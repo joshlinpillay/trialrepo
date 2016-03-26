c32ca426f0ca29cffc064f3464d36e6452bf15fc
joshlinpillay <joshlinpillay12@gmail.com>
2016-03-26 23:49:15 +0200

    3

--- Source.cpp
+++ Source.cpp
@@ -16,6 +16,13 @@ int main(int argc, char *argv[]) {
 	// 1.1 on branch "master", uncomment the following line and make a commi
 	cout << "This is the first line of code" << endl;
 
+	// 1.3 switch to the "master" branch, uncomment the following four lines and make a commit
+	 string name;
+	 cout << "Please enter your name: ";
+	 cin >> name;
+	 cout << "Hello, " << name << endl;
+
+
 
 	
 
