96690d46797c23023f7a27986ab31a5c1b364e59
joshlinpillay <joshlinpillay12@gmail.com>
2016-03-26 23:48:00 +0200

    1

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+// ***** QUESTION 1
+// Question 1 consists of 5 parts (order and approach is critical)
+
+
+int main(int argc, char *argv[]) {
+
+
+	// 1.1 on branch "master", uncomment the following line and make a commi
+	cout << "This is the first line of code" << endl;
+
+
+	
+
+}
